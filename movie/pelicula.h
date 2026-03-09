e8549a819f1d18df890f081fcb350a665b31fa0f
eduardo20072905-sketch <eduardo.20072905@gmail.com>
2026-03-08 21:26:22 -0600

    pelicula.h mod
    resuelto

--- movie/pelicula.h
+++ movie/pelicula.h
@@ -3,22 +3,30 @@
 
 #define MAX_DIRECTORES 10
 
-/* Tipo opaco */
+struct Pelicula
+{
+    char* titulo;               // Cadena dinámica para el título
+    int anio;                   // Año de estreno
+    char* genero;               // Cadena dinámica para el género
+    char* directores[MAX_DIRECTORES];  // Arreglo de cadenas dinámicas para directores
+    int numDirectores;          // Contador de directores agregados
+};
+
 typedef struct Pelicula Pelicula;
 
-/* Crear una película */
+// Crear película
 Pelicula* crearPelicula(const char* titulo, int anio, const char* genero);
 
-/* Imprimir información de la película */
+// Imprimir película
 void imprimir(Pelicula* p);
 
-/* Cambiar el género */
+// Cambiar género
 void cambiarGenero(Pelicula* p, const char* nuevoGenero);
 
-/* Agregar un director */
+// Agregar director
 void agregarDirector(Pelicula* p, const char* director);
 
-/* Liberar memoria */
+// Liberar memoria
 void destruir(Pelicula* p);
 
 #endif
